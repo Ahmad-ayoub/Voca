0357d9c73534e41b3c4ae3fb7e39a0605c9c360b
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-09-12 06:50:11 -0400

    fixing the login/signin page

--- src/Components/AuthPage.js
+++ src/Components/AuthPage.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AuthPage = () => {
   return (
     <div className="pageWrapper">
+      <h1 className="appTitle">Voca</h1>
       <div className="form loginForm">
         <h2>Login</h2>
         <input
@@ -24,13 +25,24 @@ const AuthPage = () => {
         <input
           className="form-control signupBtns"
           type="text"
+          placeholder="Name"
+        />
+        <input
+          className="form-control signupBtns"
+          type="text"
           placeholder="Username"
+        />
+        <input
+          className="form-control signupBtns"
+          type="text"
+          placeholder="Email"
         />
         <input
           className="form-control signupBtns"
           type="password"
           placeholder="Password"
         />
+        <input className="form-control signupBtns" type="date" />
         <button className="btn btn-primary mt-2 rounded-pill active">
           Signup
         </button>
