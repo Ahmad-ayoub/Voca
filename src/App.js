7c54346fb325d7b33d2587dc9c163d10eece0253
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-11-05 13:53:21 -0500

    changing the FE

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Components/AuthPage.css";
-import AuthPage from "./Components/AuthPage";
+import "./Components/Authpage/AuthPage.css";
+import AuthPage from "./Components/Authpage/AuthPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <AuthPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthPage />} index>
+          <div className="App">
+            <AuthPage />
+          </div>
+        </Route>
+        <Route path="/MainPage" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <text>Hello World</text>
+    </div>
+  );
+};
+
+export default MainPage;
