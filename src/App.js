14239cfffcf657cd3b016529b28128aeab67a6bd
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-11-13 07:01:26 -0500

    working on the UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Components/Authpage/AuthPage.css";
+import "./Components/MainPage/MainPage.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./Components/MainPage/MainPage";
 import AuthPage from "./Components/Authpage/AuthPage";
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 const MainPage = () => {
   return (
-    <div>
-      <p>Hello World</p>
+    <div className="profile_box">
+      <>
+        <FontAwesomeIcon icon={faUser} className="profile_box_image" />
+        <p className="profile_box_text">Hello World</p>
+      </>
     </div>
   );
 };
