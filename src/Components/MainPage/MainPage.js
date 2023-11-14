febd15331c4ae65b7c13982d2d4c406080a2ef24
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-11-14 05:43:02 -0500

    fixed the profile and group box

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 const MainPage = () => {
   return (
-    <div className="profile_box">
-      <>
-        <FontAwesomeIcon icon={faUser} className="profile_box_image" />
-        <p className="profile_box_text">Hello World</p>
-      </>
+    <div className="profile_and_group_box">
+      <div className="profile_box">
+        <>
+          <FontAwesomeIcon icon={faUser} className="profile_box_image" />
+          <p className="profile_box_text">User Name</p>
+        </>
+      </div>
+      <div className="group_box">
+        <>
+          <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
+          <p className="profile_box_text">Group #1</p>
+        </>
+      </div>
     </div>
   );
 };
