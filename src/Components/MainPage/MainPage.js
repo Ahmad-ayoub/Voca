0cee5447718425995e7e12eb26af279f7af184cf
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-11-15 06:59:07 -0500

    working on overall page UI for mainpage

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -9,42 +9,38 @@ import {
 const MainPage = () => {
   return (
     <div className="profile_and_group_box">
-      <div className="profile_box">
-        <>
+      <div className="group_chat_list">
+        <div className="chat_list_box">
           <FontAwesomeIcon icon={faUser} className="profile_box_image" />
           <p className="profile_box_text">User Name</p>
-        </>
-      </div>
-      <div className="group_box">
-        <>
-          <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
-          <p className="profile_box_text">Group #1</p>
-        </>
-      </div>
-      <div className="group_chat_list">
-        <>
+        </div>
+        <div className="chat_list_box">
           <FontAwesomeIcon
             icon={faMagnifyingGlass}
             className="profile_box_image"
           />
           <input type="text" placeholder="Search" aria-label="Search" />
-        </>
-        <>
+        </div>
+        <div className="chat_list_box">
           <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
           <p className="profile_box_text">Group #1</p>
-        </>
-        <>
+        </div>
+        <div className="chat_list_box">
           <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
           <p className="profile_box_text">Group #2</p>
-        </>
-        <>
+        </div>
+        <div className="chat_list_box">
           <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
           <p className="profile_box_text">Group #3</p>
-        </>
-        <>
+        </div>
+        <div className="chat_list_box">
           <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
           <p className="profile_box_text">AI Alex</p>
-        </>
+        </div>
+      </div>
+      <div className="group_box">
+        <FontAwesomeIcon icon={faUserGroup} className="profile_box_image" />
+        <p className="profile_box_text">Group #1</p>
       </div>
     </div>
   );
