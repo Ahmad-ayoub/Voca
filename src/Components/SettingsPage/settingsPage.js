3ae459a20fe60cf43b29fc38a48fbacea1553377
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-11-27 07:05:02 -0500

    working on settings page

--- src/Components/SettingsPage/settingsPage.js
+++ src/Components/SettingsPage/settingsPage.js
@@ -1,9 +1,35 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const SettingsPage = () => {
   return (
     <div>
-      <p>Hello World</p>
+      <nav>
+        <div>
+          <FontAwesomeIcon icon={faArrowLeft} />
+          <FontAwesomeIcon icon={faUser} />
+          <p>Username</p>
+        </div>
+        <button>
+          <p>Theme</p>
+        </button>
+        <button>
+          <p>Font</p>
+        </button>
+        <div>
+          <button>
+            <p>Logout</p>
+          </button>
+        </div>
+      </nav>
+      <div>
+        <FontAwesomeIcon icon={faUser} />
+        <h2>Name</h2>
+        <h2>email</h2>
+        <h2>username</h2>
+        <button>Edit</button>
+      </div>
     </div>
   );
 };
