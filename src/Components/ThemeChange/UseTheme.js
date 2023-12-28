2bd1448acd626c545bcae748549d866e10acc101
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2023-12-28 06:30:24 -0500

    working on the theme change

---
+++ src/Components/ThemeChange/UseTheme.js
@@ -0,0 +1,8 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+    const [theme, changeTheme] = useState();
