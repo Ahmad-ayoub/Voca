808e72a8aa82a2a33885507353b3f80a864d7923
Ahmad-Ayoub <ahmad.mmayoub@gmail.com>
2024-01-05 06:14:35 -0500

    added a general theme based map object

--- src/Components/ThemeChange/ThemeClasses.js
+++ src/Components/ThemeChange/ThemeClasses.js
@@ -1,3 +1,14 @@
 export const themeClasses = {
-  mainTheme: {},
+  defaultTheme: {
+    mainColor1: "mainColor1",
+    secondaryColor1: "secondaryColor1",
+  },
+  secondaryTheme: {
+    mainColor2: "mainColor2",
+    secondaryColor2: "secondaryColor2",
+  },
+  thirdTheme: {
+    mainColor3: "mainColor3",
+    secondaryColor3: "secondaryColor3",
+  },
 };
